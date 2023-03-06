0185024d380340c19e7d0180ca448ecfda890466
Luis Rios <luisbriosa@gmail.com>
2023-03-05 22:04:10 -0300

    version2

---
+++ AppFinal/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+#formularios creados. 
+
+#Formulario para regitro de reseñas.
+class CamisasFutbolFormulario(forms.Form):
+    equipo=forms.CharField(max_length=60)
+    marca=forms.CharField(max_length=60)
+    año=forms.IntegerField()
+    valoracion=forms.IntegerField()
+    reseña=forms.CharField(widget=forms.Textarea)
+    imagen=forms.ImageField()
+
+#formulario para registro de usuarios.
+class RegistrarseFormulario(UserCreationForm):
+
+    email = forms.EmailField()
+    password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Repetir la Contraseña Creada:", widget=forms.PasswordInput)
+
+    class Meta:
+
+        model = User
+        fields = ["first_name", "last_name", "username", "email", "password1", "password2"] 
+
+#formulario para edicion de usuario.
+class EditarFormulario(UserCreationForm):
+
+    email = forms.EmailField()
+    password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Repetir la Contraseña Creada:", widget=forms.PasswordInput)
+
+    class Meta:
+
+        model = User
+        fields = ["first_name", "last_name", "email", "password1", "password2"] 
